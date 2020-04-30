b93f12efa6e87a078845dcb0eece17e097531e01
USOG <usog@MacBook-Pro.local>
2020-04-30 10:37:58 +0100

    table of content

--- README.md
+++ README.md
@@ -2,29 +2,21 @@
 
 Repository contains my project for Programming and Scripting modul at GMIT. For more info [see here](https://github.com/ianmcloughlin/project-pands-2020/blob/master/project.pdf). All resources and references can be found in references section. 
 
-
-### To do list 
-- [x] ~Readme markdown~
-- [x] ~Reasearch about Ronald Fisher and his Iris dataset~
-- [x] ~Introduction~
-- [x] ~Sir Ronald Fisher~
-- [x] ~Fisher's Iris data set~
-- [x] ~Iris dataset photo~
-- [x] ~Preparation~
-- [x] ~Box plots~
-- [x] ~scatter plots~
-- [x] ~violin plots~
-- [x] ~Pair plots~
-
 ## Table of contents
-* [Introduction](#introduction)
+* [Introduction](#Introduction)
 * [Preparations](#Preparations)
+* [Dataset Summary](#Dataset-summary)
 * [Data Visualization](#Data-Visualization)
 * [Histogram](#Histogram)
+* [Box Plot](#Box-plot)
+* [Scatter Plot](#Scatter-plot)
+* [Violin plot](#Violin-plot)
+* [Pair plot](#Pair-plot)
 * [References](#References)
 
+## Introduction
 
-## Sir Ronald Aylmer Fisher (17 February 1890 – 29 July 1962) 
+### Sir Ronald Aylmer Fisher (17 February 1890 – 29 July 1962) 
 
 Sir Ronald Aylmer Fisher was a British statistican, evolutionary biologist and geneticist, born in 1890 in London and died in 1962 in Adelaide, Australia. Described by Richard Dawkins as "the father of modern and experminetal design and the greatest of Dariwn's successors". Credited for numerous aspects of experimental design and modern statistical theory. 
 
@@ -38,7 +30,7 @@ Sir Ronald Aylmer Fisher was a British statistican, evolutionary biologist and g
 - Writer of 7 books, which *Statistical Methods for Research Workers* and *The Design of Experiments* are considered to be most influential books on statistical methods of the 20th century's.
 - knighted by Queen Elizabeth in 1952, becoming Sir Ronald Aylmer Fisher.
 
-## Fisher's Iris data set
+### Fisher's Iris data set
 
 Introduced by Ronal Fisher in 1936 in his paper *The use of multiple measurements in taxonomic problems* the Iris flower data set is a multivariate data set which was used to developed a linear discriminant model. Consists of three species of Iris flower (Iris setosa, Iris virginica, Iris versicolor) and four attributes from each specie: the length and the width of the sepals and petals, in centimeters. 
 
@@ -217,5 +209,3 @@ Plots
 
 Multiple Subplots
 - [https://jakevdp.github.io/PythonDataScienceHandbook/04.08-multiple-subplots.html](https://jakevdp.github.io/PythonDataScienceHandbook/04.08-multiple-subplots.html)
-
-
--- analysis.py
+++ analysis.py
@@ -1,78 +1,214 @@
+# Maciej Izydorek 
+# GMIT G00387873
+# Fisher's Iris dataset
+# A script to analyse Iris dataset and output information about species
+
+# neccessary libraries 
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+# import iris data set
 data = pd.read_csv("iris.csv")
 
+# global variables
 colors = ('red', 'green', 'blue')
-species = ('setosa', 'versicolor', 'virginica')
+species = ('Setosa', 'Versicolor', 'Virginica')
 setosa = data[data['Species'] == 'Iris-setosa']
 versicolor = data[data['Species'] == 'Iris-versicolor']
 virginica = data[data['Species'] == 'Iris-virginica']
-attr = ('Sepal_length','Sepal_width','Petal_length','Petal_width')
+attr = ['Sepal_length','Sepal_width','Petal_length','Petal_width']
 
+# main menu
+def displayMenu():
+    print('Main Menu:')
+    print('1. Info')
+    print('2. Species')
+    print('3. Details')
+    print('4. Histogram')
+    print('5. Box plot')
+    print('6. Scatter plot')
+    print('7. Violin plot')
+    print('8. Pairplot plot')
+    print('9. Quit')
+    choice = input('Enter a number: ')
+    return choice
 
+def info():
+    # basic info shape of dataset
+    print(data.info(), '\nShape: ', data.shape)
 
+def species():
+    # Size and count of each species 
+    print(data.groupby('Species').size())
+    print(data.groupby('Species').count()) 
 
+def details():
+    # Dataset summary and attribute summary for each species. 
+    print(data.describe())
+    # save summary to txt file
+    data.describe().to_csv("summary.txt", sep=',')
+    print(data[['Sepal_length','Species']].groupby('Species').describe())
+    print(data[['Sepal_width','Species']].groupby('Species').describe())
+    print(data[['Petal_length','Species']].groupby('Species').describe())
+    print(data[['Petal_width','Species']].groupby('Species').describe())
 
+def histogram():
+    
+    data.groupby('Species').hist()
+    plt.show()
+    plt.close()
 
-data.hist(bins=15,
-                 color='steelblue',
-                 edgecolor='black', linewidth=1.0,
-                 xlabelsize=10, ylabelsize=10,
-                 figsize=(10,9),
-                 grid=False,
+def boxPlot():
 
-                 by='Species'
-                 )
+    # boxplot using sepal width across all species 
+    sns.boxplot(x="Species", y="Sepal_width", data=data)
+    # plot settings 
+    plt.title('Sepal width')
+    plt.xlabel('')
+    plt.ylabel('Sepal width (cm)')
+    # save to file 
+    plt.savefig('plots/boxplot_sepal_width.png')
+    plt.show()
+    # clean up
+    plt.close()
+ 
+    # boxplot using sepal length across all species 
+    sns.boxplot(x="Species", y="Sepal_length", data=data)
+    # plot settings 
+    plt.title('Sepal length')
+    plt.xlabel('')
+    plt.ylabel('Sepal length (cm)')
+    plt.savefig('plots/boxplot_sepal_length.png')
+    plt.show()
+    # clean up
+    plt.close()
 
+    # boxplot using petal width across all species 
+    sns.boxplot(x="Species", y="Petal_width", data=data)
+    # plot settings 
+    plt.title('Petal width')
+    plt.xlabel('')
+    plt.ylabel('Petal width (cm)')
+    plt.savefig('plots/boxplot_petal_width.png')
+    plt.show()
+    # clean up
+    plt.close()
 
+    # distribution of petal length 
+    # boxplot using petal length across all species 
+    sns.boxplot(x="Species", y="Petal_length", data=data)
+    # plot settings 
+    plt.title('Petal length')
+    plt.xlabel('')
+    plt.ylabel('Petal length (cm)')
+    plt.savefig('plots/boxplot_petal_length.png')
+    plt.show()
+    # clean up
+    plt.close()
 
-plt.show()
-# clean up
-plt.close()
-'''
-'''
-#fig, axes = plt.subplots(2,2, figsize=(12, 8))
-'''
+def scatterPlot():
 
-'''
-# boxplot using sepal width across all species 
-sns.pairplot(data, hue='Species', markers="o", 
-    plot_kws= { 'alpha': 0.8, 's': 8})
+    fig, ax = plt.subplots(figsize=(12, 6))
+    
+    # scatterplot of sepal across all species 
+    ax.scatter(setosa['Sepal_length'], setosa['Sepal_width'], marker='o', c='r', s=10, alpha=0.6, label="Setosa")
+    ax.scatter(versicolor['Sepal_length'], versicolor['Sepal_width'], marker='o', c='g', s=10, alpha=0.6, label="Versicolor")
+    ax.scatter(virginica['Sepal_length'], virginica['Sepal_width'], marker='o', c='b', s=10, alpha=0.6, label="Virginica")
+    # Scatter plot settings
+    ax.set_title('Sepal width vs length')
+    ax.legend(loc='upper left')
+    ax.set_xlabel('length(cm)')
+    ax.set_ylabel('width(cm)')
+    # save to file 
+    plt.savefig('plots/scatterplot_sepal.png')
+    plt.show()
+    plt.close()
 
-# save to file 
-#plt.savefig('plots/violinplot_iris.png')
-plt.show()
-# clean up
-plt.close()
-'''
-'''
-    for patch, color in zip(df['boxes'], colors):
-        patch.set_facecolor(color)
-'''
+    fig, ax = plt.subplots(figsize=(12, 6))
+    # scatterplot of sepal across all species 
+    ax.scatter(setosa['Petal_length'], setosa['Petal_width'], marker='o', c='r', s=10, alpha=0.6, label="Setosa")
+    ax.scatter(versicolor['Petal_length'], versicolor['Petal_width'], marker='o', c='g', s=10, alpha=0.6, label="Versicolor")
+    ax.scatter(virginica['Petal_length'], virginica['Petal_width'], marker='o', c='b', s=10, alpha=0.6, label="Virginica")
+    # Scatter plot settings
+    ax.set_title('Petal width vs length')
+    ax.legend(loc='upper left')
+    ax.set_xlabel('length(cm)')
+    ax.set_ylabel('width(cm)')
+    # save to file 
+    plt.savefig('plots/scatterplot_petal.png')
+    plt.show()
+    plt.close()
 
-'''
-ax.set_title('Sepal length')
-# df = ax.boxplot(data_to_plot, patch_artist=True)
-ax.violinplot(data_to_plot)
-ax.yaxis.grid(False)
-ax.set_ylabel('cm')
-plt.setp(ax, xticks=[y+1 for y in range(len(data_to_plot))],
-    xticklabels=['Setosa', 'Versicolor', 'Virginica'])
-plt.show()
-'''
+def violinPlot():
 
-'''
-fig, ax = plt.subplots(1,3, figsize=(18, 9))
-data.plot(kind='hist', ax=ax[0], grid=True)
-plt.show()
-'''
+    fig, axes = plt.subplots(2,2, figsize=(12, 8))
 
-'''
-# heatmap
-plt.figure(figsize=(10,11))
-sns.heatmap(data.corr())
-plt.plot()
-'''
+    # boxplot using sepal width across all species 
+    sns.violinplot(x="Species", y="Petal_width", data=data,ax=axes[0,0])
+    sns.violinplot(x="Species", y="Petal_length", data=data, ax=axes[0,1])
+    sns.violinplot(x="Species", y="Sepal_width", data=data,ax=axes[1,0])
+    sns.violinplot(x="Species", y="Sepal_length", data=data, ax=axes[1,1])
+
+    # plot settings 
+    plt.setp(axes[0, 0], 
+        xlabel='',
+        ylabel='Petal width (cm)', 
+        title='Petal width', 
+        xticklabels=['Setosa', 'Versicolor', 'Virginica'])
+    plt.setp(axes[0, 1], 
+        xlabel='',
+        ylabel='Petal length (cm)', 
+        title='Petal length', 
+        xticklabels=['Setosa', 'Versicolor', 'Virginica'])
+    plt.setp(axes[1, 0], 
+        xlabel='',
+        ylabel='Sepal width (cm)', 
+        title='Sepal width', 
+        xticklabels=['Setosa', 'Versicolor', 'Virginica'])
+    plt.setp(axes[1, 1], 
+        xlabel='',
+        ylabel='Sepal length (cm)', 
+        title='Sepal length', 
+        xticklabels=['Setosa', 'Versicolor', 'Virginica'])
+    # save to file 
+    plt.savefig('plots/violinplot_iris.png')
+    plt.show()
+    # clean up
+    plt.close()
+
+def pairPlot():
+    
+    # pairplot for whole dataset 
+    sns.pairplot(data, hue='Species', markers="o", 
+        plot_kws= {'alpha': 0.8, 's': 14})
+
+    # save to file 
+    plt.savefig('plots/pairplot_iris.png')
+    plt.show()
+    # clean up
+    plt.close()
+
+choice = displayMenu()
+while choice != '9':
+    if choice == '1':
+        info()
+    elif choice == '2':
+        species()
+    elif choice == '3':
+        details()
+    elif choice == '4':
+        histogram()
+    elif choice == '5':
+        boxPlot()
+    elif choice == '6':
+        scatterPlot()
+    elif choice == '7':
+        violinPlot()
+    elif choice == '8':
+        pairPlot()
+    elif choice != '9':
+        print('\nplease select one of the option')
+    print('\n')
+    choice = displayMenu()
